a09787d16a60130ef33bd9ae5d60bb6b6675bb79
stbraun <sb@action.ms>
2015-12-17 00:09:07 +0100

    Initial setup.py added.

---
+++ toolbox/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='toolbox',
+        version='0.1.0',
+        packages=['', 'strings', '', 'strings'],
+        package_dir={'': 'toolbox'},
+        url='',
+        license='MIT',
+        author='sb',
+        author_email='sb@action.ms',
+        description='General utilities.'
+)
